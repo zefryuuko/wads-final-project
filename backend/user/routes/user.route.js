6ed4a9d2ace53ad1a21db2fa0ef3c1f1997d6119
Zef <me@zef.sh>
2020-03-30 01:32:52 +0700

    Add and improve routes
    Added GET method implementation and use await on functions

--- backend/user/routes/user.route.js
+++ backend/user/routes/user.route.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.model');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     res.json({
         "message": "invalid request"
     });
 });
 
-router.post('/', (req, res) => {
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.find({ id: req.params.id });
+        res.status(200).send(user);
+    } catch (err) {
+        res.status(500).json({
+            "message": err
+        });
+    }
+});
+
+router.post('/', async (req, res) => {
     const user = new User ({
         id: req.body.id,
         firstName: req.body.firstName,
@@ -16,17 +27,16 @@ router.post('/', (req, res) => {
         email: req.body.email
     });
 
-    user.save()
-        .then(data => {
-            res.status(200).json({
-                "message": "User added successfully."
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                "message": err
-            });
+    try {
+        const savedUser = await user.save();
+        res.status(200).json({
+            "message": "User added successfully."
+        });  
+    } catch (err) {
+        res.status(500).json({
+            "message": err
         });
+    }
 });
 
 module.exports = router;
