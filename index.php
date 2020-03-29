65445ec5235b53a262e9d64360196b3b78103b99
David <ddsabiro@gmail.com>
2020-03-30 01:00:54 +0700

    Header, Navbar, Footer

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- REQUIRED META TAGS -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- BOOTSTRAP CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main.css">
+
+    <!-- BOOTSTRAP HOVER DOWN CSS -->
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/bootstrap-dropdownhover.min.css" rel="stylesheet">
+
+
+    <title>BinusMaya</title>
+</head>
+
+<body>
+    <!-- HEADER SECTION -->
+    <?php include("components/header.html"); ?>
+    <!-- END OF HEADER SECTION -->
+
+    <!-- NAVBAR -->
+    <?php include("components/navbar.html"); ?>
+    <!-- END OF NAVBAR -->
+
+    <h1 style="text-align: center; padding: 20px;">Hello World!</h1>
+
+    <!-- FOOTER -->
+    <?php include("components/footer.html"); ?>
+    <!-- END OF FOOTER -->
+
+    <!-- OPTIONAL JAVASCRIPT -->
+    <!-- JQUERY FIRST, THE POPPER.JS, THEN BOOTSTRAP JS -->
+    <?php include("components/javascript.html"); ?>
+</body>
+
+</html>
