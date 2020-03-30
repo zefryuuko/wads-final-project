bb38d90a287005578ebf80f656093badd5e52c34
David <ddsabiro@gmail.com>
2020-03-31 00:59:12 +0700

    Finance progress

---
+++ frontend/finance.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- REQUIRED META TAGS -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- BOOTSTRAP CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="finance.css">
+
+    <!-- BOOTSTRAP HOVER DOWN CSS -->
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/bootstrap-dropdownhover.min.css" rel="stylesheet">
+
+
+    <title>BinusMaya</title>
+</head>
+
+<body>
+    <!-- HEADER SECTION -->
+    <?php include("components/header.html"); ?>
+    <!-- END OF HEADER SECTION -->
+
+    <!-- NAVBAR -->
+    <?php include("components/navbar_finance.html"); ?>
+    <!-- END OF NAVBAR -->
+
+    <h1 style="font-family: OpenSans-Regular; padding: 10px;">FINANCIAL STATUS</h1>
+
+    <!-- VIRTUAL ACCOUNT INFORMATION -->
+    <div class="col-md-6 va-info">
+        <p>BCA VIRTUAL ACCOUNT (TUITION) - 77422531 - DAVID AMADEO <br>
+            BCA VIRTUAL ACCOUNT (NON-TUITION) - 64456233 - DAVID AMADEO</P>
+    </div>
+    <!-- END OF VIRTUAL ACCOUNT INFORMATION -->
+
+    <!-- PERIOD -->
+
+
+
+    <!-- FOOTER -->
+    <?php include("components/footer.html"); ?>
+    <!-- END OF FOOTER -->
+
+    <!-- OPTIONAL JAVASCRIPT -->
+    <!-- JQUERY FIRST, THE POPPER.JS, THEN BOOTSTRAP JS -->
+    <?php include("components/javascript.html"); ?>
+</body>
+
+</html>
