c060d782afc96ae60187c708f30b60b1a8ea57d8
Zef <me@zef.sh>
2020-04-01 12:14:06 +0700

    Change MySQL library
    Switched from mysql to mysql2 for better async/await support

--- backend/auth/db.js
+++ backend/auth/db.js
@@ -1,8 +1,9 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 require('dotenv/config');
 
 var pool = mysql.createPool({
     host            : process.env.MYSQL_HOST,
+    port            : process.env.MYSQL_PORT,
     user            : process.env.MYSQL_USER,
     password        : process.env.MYSQL_PASS,
     database        : process.env.MYSQL_DB,
