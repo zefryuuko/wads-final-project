67d57edb59d9e35ac34006c063f910b98496406e
Zef <me@zef.sh>
2020-04-03 23:21:43 +0700

    Implement session model
    Implemented session handling methods

---
+++ backend/auth/model/session.model.js
@@ -0,0 +1,73 @@
+const db = require('../db');
+const { uuid } = require('uuidv4');
+const dotenv = require('dotenv/config');
+
+class Session {
+    async addSession(accountId, rememberMe) {
+        try {
+            const sessionId = uuid();
+            const result = await db.query(
+                'INSERT INTO sessions (session_id, account_id, expiry_time, remember_me) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 1 HOUR), ?)',
+                [
+                    sessionId,
+                    accountId,
+                    !rememberMe ? 0 : 1
+                ]
+            );
+            if (result.length > 0) return sessionId;
+            else return undefined;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async sessionExists(sessionId) {
+        try {
+            const result = await db.query(
+                'SELECT session_id FROM sessions WHERE session_id = ? AND NOW() < expiry_time',
+                [
+                    sessionId
+                ]
+            );
+            if (result[0].length == 0) {
+                await this.revokeSession(sessionId);
+                return false;
+            }
+            else return true;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async revokeSession(sessionId) {
+        try {
+            const result = await db.query(
+                'DELETE FROM sessions WHERE session_id = ?',
+                [
+                    sessionId
+                ]
+            );
+            if (result[0].affectedRows > 0) return true;
+            else return false;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async revokeSessions(accountId) {
+        try {
+            const result = await db.query(
+                'DELETE FROM sessions WHERE account_id = ?',
+                [
+                    sessionId
+                ]
+            );
+            if (result[0].affectedRows > 0) return true;
+            else return false;
+        } catch (err) {
+            throw err;
+        }
+    }
+}
+
+module.exports = new Session();
