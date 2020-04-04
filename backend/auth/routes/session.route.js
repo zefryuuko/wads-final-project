5366a15c8b8c02438e6eb20542d99194a3bef58b
Zef <me@zef.sh>
2020-04-04 23:10:53 +0700

    Implement session routing
    Implemented session id generation, validation and deletion routes

--- backend/auth/routes/session.route.js
+++ backend/auth/routes/session.route.js
@@ -1,18 +1,92 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const account = require('../model/account.model');
+const session = require('../model/session.model');
 
-router.get('/', (req, res) => {
-    res.json({
-        "message": "Invalid request"
-    });
+router.get('/', async (req, res) => {
+    const { sessionId } = req.body;
+
+    if (!sessionId) {
+        res.status(400).json({
+            "message": "Missing parameter(s)"
+        });
+        return;
+    }
+
+    try {
+        const isValidSession = await session.sessionExists(sessionId);
+        if (!isValidSession) {
+            res.status(403).json( {
+                "message": "Invalid session"
+            });
+            return;
+        }
+        res.json({
+            "message": "Session is valid"
+        });
+    } catch (err) {
+        res.status(500).json( {
+            "message": `${err}`
+        });
+    }
 });
 
-router.post('/', (req, res) => {
-    res.json({
-        "message": "Received POST on /session",
-        "postData": req.body
-    });
+router.post('/', async (req, res) => {
+    const { emailAddress, password, rememberMe } = req.body;
+
+    if (!(emailAddress && password)) {
+        res.status(400).json({
+            "message": "Missing parameter(s)"
+        });
+        return;
+    }
+    
+    try {
+        const accountId = await account.getAccountId(emailAddress, password);
+        if (!accountId) {
+            res.status(401).json({
+                "message": "Invalid email and/or password"
+            });
+            return;
+        }
+        const sessionId = await session.addSession(accountId, rememberMe);
+        res.json({
+            "message": "Logged in successfully",
+            "sessionId": sessionId
+        });
+    } catch (err) {
+        res.status(500).json( {
+            "message": `${err}`
+        });
+    }
+});
+
+router.delete('/', async (req, res) => {
+    const { sessionId, revokeAll } = req.body;
+
+    if (!sessionId) {
+        res.status(400).json({
+            "message": "Missing parameter(s)"
+        });
+        return;
+    }
+
+    try {
+        const isRevoked = revokeAll ? await session.revokeSessions(sessionId) : await session.revokeSession(sessionId);
+        if (!isRevoked) {
+            res.status(404).json({
+                "message": "Session not found"
+            });
+            return;
+        }
+        res.json({
+            "message": "Session(s) revoked successfully"
+        });
+    } catch (err) {
+        res.status(500).json( {
+            "message": `${err}`
+        });
+    }
 });
 
 module.exports = router;
