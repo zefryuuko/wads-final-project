d4a42f51477e0f56e57cabbecb78dbbbfe37fe66
Zef <me@zef.sh>
2020-04-12 17:13:11 +0700

    Update course model

--- backend/courses/models/course.model.js
+++ backend/courses/models/course.model.js
@@ -2,25 +2,28 @@ const mongoose = require('mongoose');
 
 const courseSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    group: mongoose.Schema.Types.ObjectId,
-    code: String,
-    name: String,
-    description: String,
+    group: { type: mongoose.Schema.Types.ObjectId, required: true },
+    code: { type: String, required: true, uppercase: true, unique: true, dropDups: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
     learningOutcomes: [String],
-    textbooks: [
-        {
-            title: String,
-            author: String,
-            year: Number,
-            publisher: String,
-            ISBN: String
-        }],
-    evaluation: [{
-            name: String,
-            evaluatedLearningOutcomes: [Boolean],
-            weight: Number
-        }],
-    scu: Number
+    scu: { type: Number, required: true },
+    class: [{
+        code: { type: String, required: true, uppercase: true, unique: true, dropDups: true },
+        textbooks: [
+            {
+                title: { type: String, required: true },
+                author: String,
+                year: Number,
+                publisher: String,
+                isbn: String
+            }],
+        evaluation: [{
+                name: { type: String, required: true },
+                evaluatedLearningOutcomes: { type: [Boolean], required: true },
+                weight: { type: Number, required: true },
+            }],
+    }],
 });
 
 module.exports = mongoose.model('Course', courseSchema);
