42ff66aeaaa8453c51accffeed1097ab5b0e6a55
Zef <me@zef.sh>
2020-05-01 19:16:25 +0700

    Implement class CRUD routes

--- backend/class/routes/semester.route.js
+++ backend/class/routes/semester.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Major = require('../models/major.model');
 const Semester = require('../models/semester.model');
+const Class = require('../models/class.model')
 
 router.get('/', async (req, res) => {
     // TODO: Add pagination
@@ -10,6 +11,10 @@ router.get('/', async (req, res) => {
         "message": "invalid request"
     });
 });
+
+//
+//  Semester routes
+//
 
 router.get('/:id', async (req, res) => {
     try {
@@ -145,7 +150,10 @@ router.delete('/:id', async (req, res) => {
         );
         
         // Remove reference to the semester from major
-        const mResult = await Major.updateOne({ _id: semester[0].majorId }, { $pull: { linkedSemesters: req.params.id } });
+        const mResult = await Major.updateOne(
+            { _id: semester[0].majorId }, 
+            { $pull: { linkedSemesters: req.params.id } }
+            );
         
         // Remove semester data
         const removedSemester = await Semester.deleteOne({ _id: req.params.id });
@@ -170,6 +178,198 @@ router.delete('/:id', async (req, res) => {
 
         res.status(500).json({
             "message": err
+        });
+    }
+});
+
+//
+//  Class routes
+//
+
+router.get('/:id/:classCode/:courseCode', async (req, res) => {
+    try {
+        const semester = await Semester.aggregate([
+            { 
+                $match: {
+                    _id: req.params.id,
+                    classes: {
+                        $elemMatch: {
+                            classCode: { $eq: req.params.classCode },
+                            courseCode: { $eq: req.params.courseCode }
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    'classes.classCode': 1      // FOR FUCK SAKE PLS WORK
+                }
+            }
+        ]);
+        
+        if (!semester) {
+            res.status(404).json({
+                "message": 'One or more parameters are not found'
+            });
+        }
+        else {
+            res.status(200).send(semester);
+        }
+    } catch (err) {
+        if (err.name == "CastError") {
+            res.status(404).json({
+                "message": `Semester with id ${req.params.id} is not found.`
+            });
+            return;
+        }
+
+        res.status(500).json({
+            "message": `${err}`
+        });
+    }
+});
+
+router.post('/:id', async (req, res) => {
+    try {
+        // Check if semester id exists
+        const semester = await Semester.find(
+            { _id: req.params.id },
+            { __v: 0 }
+        );
+        
+        if (semester.length < 1) {
+            res.status(404).json({
+                "message": `Semester with id ${req.body.majorId} does not exist`
+            });
+            return;
+        }
+
+        // TODO: Validate course code
+
+        // Add class data
+        req.body._id = mongoose.Types.ObjectId();
+        const newClass = new Class(req.body);
+        const result = await Semester.updateOne(
+            { _id: req.params.id }, 
+            { $push: { classes: newClass } }
+        );
+
+        res.status(200).json({
+            "message": "Class added successfully.",
+            "classId": req.body._id
+        });  
+    } catch (err) {
+        // Handle missing parameters
+        if (err.name == 'ValidationError') {
+            const missingKey = Object.keys(err.errors)[0];
+            res.status(400).json({
+                'message': `Missing '${missingKey}' parameter`,
+                'details': `${err}`
+            });
+            return;
+        }
+
+        // Handle duplicate key error
+        if (err && err.code === 11000) {
+            const duplicateKey = Object.keys(err.keyValue)[0];
+            res.status(409).json({
+                'message': `${err.keyValue[duplicateKey]} already exists`,
+            });
+            return;
+        }
+        
+        // Handle invalid object ids
+        if (err.name == "CastError") {
+            res.status(404).json({
+                "message": `Class with id ${req.body.majorId} does not exist`
+            });
+            return;
+        }
+
+        // Respond with internal server error
+        res.status(500).json({
+            'message': `${err}`
+        });
+    }
+});
+
+router.patch('/:id/:classCode/:courseCode', async (req, res) => {
+    try {
+        // Update class
+        const result = await Semester.findOneAndUpdate(
+            { 
+                _id: req.params.id, 
+                'classes.classCode': { $eq: req.params.classCode }, 
+                'classes.courseCode': { $eq: req.params.courseCode } 
+            }, 
+            { $setOnInsert: { 'classes.$': req.body } }
+        );
+
+        if (!result) {
+            res.status(404).json({
+                'message': 'One or more parameters are not found'
+            });
+            return;
+        }
+
+        res.json({
+            'message': 'Class updated successfully'
+        });
+    } catch (err) {
+        if (err.name == "CastError") {
+            res.status(404).json({
+                "message": `Semester with id ${req.params.id} is not found.`
+            });
+            return;
+        }
+
+        // Respond with internal server error
+        res.status(500).json({
+            'message': `${err}`
+        });
+    }
+});
+
+router.delete('/:id/:classCode/:courseCode', async (req, res) => {
+    try {
+        // Remove class data
+        const removedSemester = await Semester.findOneAndUpdate(
+            { 
+                _id: req.params.id, 
+                'classes.classCode': { $eq: req.params.classCode }, 
+                'classes.courseCode': { $eq: req.params.courseCode } 
+            }, 
+            { 
+                $pull: {
+                    'classes': {
+                        'classCode': req.params.classCode, 
+                        'courseCode': req.params.courseCode
+                    }
+                } 
+            },
+            { new: true }
+        );
+
+        if (!removedSemester) {
+            res.status(404).json({
+                "message": `One or more parameters are not found.`
+            });
+        }
+        else {
+            res.status(200).json({
+                "message": "Class deleted successfully."
+            });
+        }
+    } catch (err) {
+        if (err.name == "CastError") {
+            res.status(404).json({
+                "message": `Semester with id ${req.params.id} is not found.`
+            });
+            return;
+        }
+
+        res.status(500).json({
+            "message": `${err}`
         });
     }
 });
