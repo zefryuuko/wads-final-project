e660c07ded2195615a51bf5c4260291ace3acaa6
Zef <me@zef.sh>
2020-05-07 00:41:31 +0700

    Load user name from backend

--- frontend/src/app/shared/services/user/user.service.ts
+++ frontend/src/app/shared/services/user/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getUserDetails(userId: string) {
+    const url = `${environment.apiEndpoint}/users/user/${userId}`;
+    return this.httpClient.get(url);
+  }
 }
