2d56eca2a87b4025b3af4feb47fa769c1409c144
David <ddsabiro@gmail.com>
2020-05-07 02:20:32 +0700

    Add each course pages
    Each course page is the next page the user will be directed to after choosing which course they would like to see the details to.

---
+++ frontend-temporary/each_course.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- REQUIRED META TAGS -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- BOOTSTRAP CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="each_course.css">
+
+    <!-- BOOTSTRAP HOVER DOWN CSS -->
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/bootstrap-dropdownhover.min.css" rel="stylesheet">
+
+
+    <title>BinusMaya</title>
+</head>
+
+<body>
+    <!-- HEADER SECTION -->
+    <?php include("components/header.html"); ?>
+    <!-- END OF HEADER SECTION -->
+
+    <!-- NAVBAR -->
+    <?php include("components/navbar_courses.html"); ?>
+    <!-- END OF NAVBAR -->
+
+    <div class="row row-title" style="margin: 0px !important;">
+        <div class="col-md-12 va-info">
+            <h1 style="font-family: OpenSans-Bold;">COURSES</h1>
+        </div>
+    </div>
+    <div class="body-content">
+        <div class="row" style="margin: 0px !important;">
+            <div class="col-md-12 va-info">
+                <h3 style="font-family: OpenSans-Regular;">COMP 6343</h3>
+                <h1 style="font-family: OpenSans-Regular">WEB APPLICATION AND SECURITY</h1>
+            </div>
+        </div>
+        <div class="row" style="margin: 0px !important;">
+            <div class="col-md-8 va-info">
+                <div class="row nav_desktop" style="padding: 5px 0px;">
+                    <div class="col-md-12" style="margin: 0px !important;">
+                        <nav class="navbar navbar-expand-lg navbar-light" style="padding: 0px !important;">
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav mr-auto">
+                                    <li class="nav-item" style="border-right: 1px solid #cccccc; border-left: 1px solid #cccccc; padding: 5px 40px;">
+                                        <a class="nav-link" href="#">COURSE INFO</a>
+                                    </li>
+                                    <li class="nav-item" style="border-right: 1px solid #cccccc; padding: 5px 40px;">
+                                        <a class="nav-link" href="">RESOURCES</a>
+                                    </li>
+                                    <li class="nav-item" style="border-right: 1px solid #cccccc; padding: 5px 40px;">
+                                        <a class="nav-link" href="#">SHARED MATERIALS</a>
+                                    </li>
+                                    <li class="nav-item" style="border-right: 1px solid #cccccc; padding: 5px 40px;">
+                                        <a class="nav-link" href="#">ASSIGNMENT</a>
+                                    </li>
+                                    <li class="nav-item" style="border-right: 1px solid #cccccc; padding: 5px 40px;">
+                                        <a class="nav-link" href="#">PEOPLE</a>
+                                    </li>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row" style="margin: 0px !important;">
+            <div class="col-md-8 va-info">
+                <h5 style="font-family: OpenSans-Regular;">Description</h5>
+                <p style="font-family: OpenSans-Light;">This course is designed to teach the major web-related topics with several technologies as a unifying theme. It introduces students to the concepts and techniques of a dynamic web page construction, basic web protocols, explore design issues and techniques, and its implementation in Scripting language and Java server–side programming. Students will be exposed to development using those programming languages in solving common problems in the areas of development, and systems administration on a particular operating systems platform.</p>
+                <p style="font-family: OpenSans-Light">In previous computer science courses, students gain experience in a wide variety of 'traditional' programming languages in procedural, declarative and object-oriented flavors. This course addresses the design of languages for contemporary computing applications, in particular scripting languages, which are an area of great topical interest. The study of web programming languages and their applications introduces the student to new ways of programming and leads on to the area of component-based software, a technology of great importance to the next generation of software systems including computer games and web development.</p>
+            </div>
+        </div>
+        <div class="row" style="margin: 0px !important;">
+            <div class="col-md-8 va-info">
+                <h5 style="font-family: OpenSans-Regular;">Learning Outcomes</h5>
+            </div>
+        </div>
+        <div class="row" style="margin: 0px !important;">
+            <div class="col-md-8 va-info">
+                <h5 style="font-family: OpenSans-Regular;">Scoring</h5>
+            </div>
+        </div>
+        <div class="row" style="margin: 0px !important;">
+            <div class="col-md-8 va-info">
+                <h5 style="font-family: OpenSans-Regular;">Textbooks</h5>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+    </div>
+
+    <!-- FOOTER -->
+    <?php include("components/footer.html"); ?>
+    <!-- END OF FOOTER -->
+
+    <!-- OPTIONAL JAVASCRIPT -->
+    <!-- JQUERY FIRST, THE POPPER.JS, THEN BOOTSTRAP JS -->
+    <?php include("components/javascript.html"); ?>
+</body>
+
+</html>
