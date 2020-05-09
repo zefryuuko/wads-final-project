09e2a72ea5baceb300fe88b57da6147e46f255a8
Zef <me@zef.sh>
2020-05-10 01:07:13 +0700

    Fix unused dependency warning

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import AuthService from '../services/AuthService';
 
 class Logout extends React.Component {  
     render() {
-        // AuthService.logout().then((res) => {});
         return (
             <script>{localStorage.clear()} {window.location.href = "/"}</script>
         );
