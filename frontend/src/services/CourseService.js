bf1b939a6c22cbd3527af510406becde6c770552
Zef <me@zef.sh>
2020-05-10 16:40:05 +0700

    Add CourseService.js

---
+++ frontend/src/services/CourseService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+import env from '../environments/dev.env';
+
+class UserService {
+    API_ENDPOINT = `${env.API_ENDPOINT}courses`;
+
+    async getCourseGroup(page, callback) {
+        try {
+            const res = await axios.get(`${this.API_ENDPOINT}/groups/?page=${page}`);
+            if (callback) callback(res.data);
+            return res.data;
+        } catch (err) {
+            if (callback) callback(err);
+            return err;
+        }
+    }
+}
+
+export default new UserService();
