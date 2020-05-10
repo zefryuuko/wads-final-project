8a15699ac45788729a7c41a8c5f751b330b31ab9
Zef <me@zef.sh>
2020-05-10 16:40:44 +0700

    Get data from backend

--- frontend/src/components/staff/StaffCoursesList.js
+++ frontend/src/components/staff/StaffCoursesList.js
@@ -3,11 +3,16 @@ import {Redirect} from 'react-router-dom';
 
 // Services
 import AuthService from '../../services/AuthService';
+import CourseService from '../../services/CourseService';
 
 // UI Elements
 import MainWrapper from '../ui-elements/MainWrapper';
 import PageWrapper from '../ui-elements/PageWrapper';
+import ContentWrapper from '../ui-elements/ContentWrapper';
 import PageBreadcrumb from '../ui-elements/PageBreadcrumb';
+import Card from '../ui-elements/Card';
+import Table from '../ui-elements/Table';
+import Button from '../ui-elements/Button';
 
 // Components
 import Preloader from '../Preloader';
@@ -20,12 +25,21 @@ class StaffCourseList extends React.Component {
         super();
         this.state = {
             isLoading: true,
-            isLoggedIn: false
+            isLoggedIn: false,
+            currentTablePage: 1,
+            currentTableContent: []
         }
 
         // Set page display mode when loading
         this.loadingStyle = {display: "none"}
         this.loadedStyle = {display: ""}
+
+        // Bind functions
+        this.reloadTable = this.reloadTable.bind(this);
+    }
+
+    reloadTable() {
+        this.setState({currentTableContent: []});
     }
     
     componentDidMount() {
@@ -43,6 +57,12 @@ class StaffCourseList extends React.Component {
                         isLoggedIn: true
                     })
             });
+        
+        // Load table content
+        CourseService.getCourseGroup(this.state.currentTablePage).then(res => {
+            // TODO: add error validation
+            this.setState({currentTableContent: res});
+        })
     }
 
     render() {
@@ -55,7 +75,28 @@ class StaffCourseList extends React.Component {
                     <Sidebar />
                     <PageWrapper>
                         <PageBreadcrumb title="Courses" root="Course Administration"/>
-                        <h1>Staff Courses Body</h1>
+                        <ContentWrapper>
+                            <div className="row">
+                                <div className="col-12">
+                                    <Card>
+                                        <Table header={["Code", "Name", "Actions"]}>
+                                            {this.state.currentTableContent.length > 0 ? this.state.currentTableContent.map(row => {
+                                                return (
+                                                    <tr key={row.prefix}>
+                                                        <th scope="row">{row.prefix}</th>
+                                                        <td>{row.name}</td>
+                                                        <td style={{width: "150px", minWidth: "150px"}}>
+                                                            <Button className="btn btn-sm btn-secondary btn-sm mr-2">Edit</Button>
+                                                            <Button className="btn btn-sm btn-danger">Delete</Button>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            }): <tr><td colSpan="3" align="center">Data not loaded arrrgh. <a href="#reload" onClick={e => {e.preventDefault(); this.reloadTable();}}>Try reloading?</a></td></tr>}
+                                        </Table>
+                                    </Card>
+                                </div>
+                            </div>
+                        </ContentWrapper>
                         <Footer />
                     </PageWrapper>
                 </div>
