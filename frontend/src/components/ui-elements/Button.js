594b30c089519c000c1a6680cac3f6124160597a
Zef <me@zef.sh>
2020-05-15 19:57:42 +0700

    Fix incorrect tag properties

--- frontend/src/components/ui-elements/Button.js
+++ frontend/src/components/ui-elements/Button.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Button extends Component {
     render() {
         const {tag, loading, children, disabled, ...props} = this.props;
-        const spinner = <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        const spinner = <span className="spinner-border spinner-border-sm mr-1" role="status" aria-hidden="true"></span>
         return tag === "a" ? <a {...props} disabled={disabled ? disabled : loading ? loading : false}>{loading ? spinner : null}{children}</a>
         :tag === "div" ? <div {...props} disabled={disabled ? disabled : loading ? loading : false}>{loading ? spinner : null}{children}</div>
         :<button {...props} disabled={disabled ? disabled : loading ? loading : false}>{loading ? spinner : null}{children}</button>
