707693add86a8cd5df7329db339cac16507b1b86
Zef <me@zef.sh>
2020-05-18 00:36:24 +0700

    Allow custom cancel button text

--- frontend/src/components/ui-elements/ModalFooter.js
+++ frontend/src/components/ui-elements/ModalFooter.js
@@ -4,7 +4,7 @@ class ModalHeader extends Component {
     render() { 
         return (
             <div className="modal-footer">
-                <button type="button" className="btn btn-light" data-dismiss="modal" disabled={this.props.disableClose ? this.props.disableClose : false}>Cancel</button>
+                <button type="button" className="btn btn-light" data-dismiss="modal" disabled={this.props.disableClose ? this.props.disableClose : false}>{this.props.buttonText ? this.props.buttonText : "Cancel"}</button>
                 {this.props.children}
             </div>
         );
