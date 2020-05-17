f49711e381ccaf3f6ebf1e9aac24f0f92ce89fe5
Zef <me@zef.sh>
2020-05-17 17:11:01 +0700

    Allow custom styles on card

--- frontend/src/components/ui-elements/Card.js
+++ frontend/src/components/ui-elements/Card.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Card extends Component {
     render() { 
         return (
-            <div className="card">
+            <div className="card" style={this.props.style ? this.props.style : {}}>
                 {this.props.padding ? 
                     <div className="card-body">
                         <div className="float-right">{this.props.right ? this.props.right : null}</div>
