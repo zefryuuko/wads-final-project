1ab9f6cc6470987a50887413ba4a9b6617b7ff3f
Zef <me@zef.sh>
2020-05-17 23:55:32 +0700

    Implement new evaluation rendering method

--- frontend/src/components/ui-elements/Evaluation.js
+++ frontend/src/components/ui-elements/Evaluation.js
@@ -6,21 +6,43 @@ class Textbooks extends Component {
     render() { 
         return (
             <div>
-                <div className="card-title">Evaluations<span className="float-right" style={{fontWeight: "initial"}}>{this.props.right ? this.props.right : null}</span></div>
-                    {this.props.data && (this.props.data.length > 0) ?
-                    <Table header={Array.prototype.concat(["Component"], this.props.data[0].evaluatedLearningOutcomes.map((element, index) => {
-                        return `LO${index + 1}`
-                    }))}>
-                        {this.props.data.map((element, index) => {
-                            return <tr key={index}>
-                                <th scope="row">{element.name} ({element.weight}%)</th>
-                                {element.evaluatedLearningOutcomes.map((isEvaluated, index) => {
-                                    return <td key={index}>{isEvaluated ? <span><i className="icon-check"/></span> : <span>-</span>}</td>
-                                })}
-                            </tr>
-                        })}
-                    </Table>
-                    : <div style={{textAlign: "center"}}>No evaluation</div>}
+                {this.props.loData && this.props.data.length > 0 ?    // New rendering method
+                    <div>
+                        <div className="card-title">Evaluations<span className="float-right" style={{fontWeight: "initial"}}>{this.props.right ? this.props.right : null}</span></div>
+                        <Table header={Array.prototype.concat(["Component"], this.props.loData.length > 0 ? this.props.loData.map((element, index) => {
+                            return `LO${index + 1}`
+                        }) : [])}>
+                            {this.props.data.map((element, index) => {
+                                return <tr key={index}>
+                                    <th scope="row">{element.name} ({element.weight}%)</th>
+                                    {this.props.loData.map((lo, loId) => {
+                                        return <td key={loId}>
+                                            {element.evaluatedLearningOutcomes[loId] !== undefined ? element.evaluatedLearningOutcomes[loId] ? <span><i className="icon-check"/></span> : <span>-</span> : <span>-</span>}
+                                        </td>
+                                    })}
+                                </tr>
+                            })}
+                        </Table>
+                    </div>
+                :                                           // Legacy rendering
+                    <div>
+                        <div className="card-title">Evaluations<span className="float-right" style={{fontWeight: "initial"}}>{this.props.right ? this.props.right : null}</span></div>
+                        {this.props.data && (this.props.data.length > 0) ?
+                        <Table header={Array.prototype.concat(["Component"], this.props.data[0].evaluatedLearningOutcomes.map((element, index) => {
+                            return `LO${index + 1}`
+                        }))}>
+                            {this.props.data.map((element, index) => {
+                                return <tr key={index}>
+                                    <th scope="row">{element.name} ({element.weight}%)</th>
+                                    {element.evaluatedLearningOutcomes.map((isEvaluated, index) => {
+                                        return <td key={index}>{isEvaluated ? <span><i className="icon-check"/></span> : <span>-</span>}</td>
+                                    })}
+                                </tr>
+                            })}
+                        </Table>
+                        : <div style={{textAlign: "center"}}>No evaluation</div>}
+                    </div>
+                }
             </div>
         );
     }
