00b1dc7bf1b8cfa11be890f9daf933401cd9242d
Zef <me@zef.sh>
2020-05-17 17:10:46 +0700

    Add user get methods

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -4,6 +4,50 @@ import env from '../environments/dev.env';
 
 class UserService {
     API_ENDPOINT = `${env.API_ENDPOINT}users`;
+    
+    async getUsers(callback) {
+        try {
+            const res = await axios.get(`${this.API_ENDPOINT}/user`);
+            if (callback) callback(res.data);
+            return res.data;
+        } catch (err) {
+            if (callback) callback(err);
+            throw err;
+        }
+    }
+
+    async getStaffs(callback) {
+        try {
+            const res = await axios.get(`${this.API_ENDPOINT}/user?staff=1}`);
+            if (callback) callback(res.data);
+            return res.data;
+        } catch (err) {
+            if (callback) callback(err);
+            throw err;
+        }
+    }
+
+    async getLecturers(callback) {
+        try {
+            const res = await axios.get(`${this.API_ENDPOINT}/user?lecturer=1}`);
+            if (callback) callback(res.data);
+            return res.data;
+        } catch (err) {
+            if (callback) callback(err);
+            throw err;
+        }
+    }
+
+    async getStudents(callback) {
+        try {
+            const res = await axios.get(`${this.API_ENDPOINT}/user?student=1}`);
+            if (callback) callback(res.data);
+            return res.data;
+        } catch (err) {
+            if (callback) callback(err);
+            throw err;
+        }
+    }
 
     async getUserData(callback) {
         try {
@@ -14,6 +58,17 @@ class UserService {
         } catch (err) {
             if (callback) callback(err);
             return err;
+        }
+    }
+
+    async getUserById(universalId, callback) {
+        try {
+            const res = await axios.get(`${this.API_ENDPOINT}/user/${universalId}`);
+            if (callback) callback(res.data);
+            return res.data;
+        } catch (err) {
+            if (callback) callback({}, err);
+            throw err;
         }
     }
 }
