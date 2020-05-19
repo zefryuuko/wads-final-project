da77223f33ecc1aabc8631cbffff1c3049eba0c4
Zef <me@zef.sh>
2020-05-19 13:08:07 +0700

    Shut up eslint

--- frontend/src/components/staff/user-accounts/Account.js
+++ frontend/src/components/staff/user-accounts/Account.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Redirect, Link, withRouter} from 'react-router-dom';
+import {Redirect, withRouter} from 'react-router-dom';
 
 // Services
 import AuthService from '../../../services/AuthService';
