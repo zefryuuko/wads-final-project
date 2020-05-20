77d54c3bed71663974354404ef42d9232b158c6d
Zef <me@zef.sh>
2020-05-21 01:42:16 +0700

    Implement basic dash for lecturer

---
+++ frontend/src/components/lecturer/Dashboard.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import {Redirect, Link} from 'react-router-dom';
+
+// Services
+import AuthService from '../../services/AuthService';
+import UserService from '../../services/UserService';
+import ClassService from '../../services/ClassService';
+
+// UI Elements
+import PageWrapper from '../ui-elements/PageWrapper';
+import PageBreadcrumb from '../ui-elements/PageBreadcrumb';
+import ContentWrapper from '../ui-elements/ContentWrapper';
+import Breadcrumb from '../ui-elements/Breadcrumb';
+import Card from '../ui-elements/Card';
+
+class StaffDashboard extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoading: true,
+            isAuthenticating: true,
+            isAuthenticated: false,       // TODO: revalidate auth
+            userFirstName: "",
+            userFirstFullName: "",
+            userLastName: "",
+            accountDetails: undefined,
+            currentEnrolledSemester: undefined,
+        }
+
+        // Set page display mode when loading
+        this.loadingStyle = {visibility: "none"}
+        this.loadedStyle = {visibility: "visible", opacity: 1}
+    }
+    
+    componentDidMount() {
+        // Perform session check
+        AuthService.isLoggedIn()
+            .then(res => {
+                if (res.response && (res.response.status === 403))
+                    this.setState({
+                        isAuthenticating: false,
+                        isAuthenticated: false
+                    });
+                else
+                    this.setState({
+                        isAuthenticating: false,
+                        isAuthenticated: true
+                    })
+            });
+        
+        // Load user info
+        UserService.getUserData()
+            .then(res => {
+                if (res.firstName)
+                this.setState({
+                    userFirstName: res.firstName.split(' ')[0],
+                    userFirstFullName: res.firstName,
+                    userLastName: res.lastName
+                })
+            });
+
+        UserService.getUserAccountDetails(localStorage.getItem('universalId'), localStorage.getItem('activeAccount').split(",")[0])
+            .then(res => {
+                this.setState({
+                    accountDetails: res
+                })
+            });
+
+            // Load classes data
+            ClassService.getCourseByLecturerId(localStorage.getItem('universalId'))
+            .then(res => {
+                this.setState({currentEnrolledSemester: res[res.length - 1], isLoading: false});
+            }).catch(err => {
+                this.setState({isLoading: false})
+            });
+
+    }
+    
+    render() {
+        if (!this.state.isAuthenticated && !this.state.isAuthenticating) return <Redirect to="/"/>
+        return (
+            <div className="ease-on-load" style={this.state.isLoading ? this.loadingStyle : this.loadedStyle}>
+                <PageWrapper>
+                    <PageBreadcrumb title={`Welcome Back, ${this.state.userFirstName}!`} breadcrumb={<Breadcrumb current="Dashboard"/>}/>
+                    <ContentWrapper>
+                        <div className="card-group">
+                            <div className="card border-right">
+                                <div className="card-body">
+                                    <div className="d-flex d-lg-flex d-md-block align-items-center">
+                                        <div>
+                                            <div className="d-inline-flex align-items-center">
+                                                <h2 className="text-dark mb-1 font-weight-medium">2</h2>
+                                            </div>
+                                            <h6 className="text-muted font-weight-normal mb-0 w-100 text-truncate">Courses this semester</h6>
+                                        </div>
+                                        <div className="ml-auto mt-md-3 mt-lg-0">
+                                            <span className="opacity-7 text-muted"><i data-feather="edit-3" className="feather-icon"></i></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card border-right">
+                                <div className="card-body">
+                                    <div className="d-flex d-lg-flex d-md-block align-items-center">
+                                        <div>
+                                            <h2 className="text-dark mb-1 w-100 text-truncate font-weight-medium">{this.state.accountDetails && this.state.accountDetails.metadata.currentGPA ? this.state.accountDetails.metadata.currentGPA : "N/A"}</h2>
+                                            <h6 className="text-muted font-weight-normal mb-0 w-100 text-truncate">Classes today
+                                            </h6>
+                                        </div>
+                                        <div className="ml-auto mt-md-3 mt-lg-0">
+                                            <span className="opacity-7 text-muted"><i data-feather="frown" className="feather-icon"></i></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12 col-lg-12">
+                                <Card title="Today's Schedule" padding>
+
+                                </Card>
+                            </div>
+                        </div>
+                        <Card title="This Semester" padding>
+                            <div className="list-group">
+                                {this.state.currentEnrolledSemester ?
+                                    this.state.currentEnrolledSemester.classes.map((element, index) => {
+                                        return <Link to={`/lecturer/courses/${this.state.currentEnrolledSemester._id}/${element.classCode}/${element.courseCode}`} key={index} className="list-group-item">
+                                            <span className="mr-2">{element.metadata.name}</span> 
+                                            <span className="badge badge-primary mr-1">{element.classType}</span>
+                                            <span className="badge badge-secondary mr-1">{element.classCode}</span>
+                                            <span className="badge badge-secondary mr-1">{element.courseCode}</span>
+                                        </Link>
+                                    })
+                                : <div style={{textAlign: "center"}}>No data</div>}
+                            </div>
+                        </Card>
+                    </ContentWrapper>
+                </PageWrapper>
+            </div>
+        );
+    }
+}
+
+export default StaffDashboard;
---
+++ frontend/src/components/lecturer/LecturerDashboardPage.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
+
+// Services
+import AuthService from '../../services/AuthService';
+
+// UI Elements
+import MainWrapper from '../ui-elements/MainWrapper';
+import PageWrapper from '../ui-elements/PageWrapper';
+
+// Components
+import Preloader from '../Preloader';
+import Navbar from '../Navbar';
+import Sidebar from './Sidebar'
+import Footer from '../Footer';
+
+// Course Administration
+import Dashboard from './Dashboard';
+import Courses from './courses/Courses';
+import Course from './courses/Course';
+
+class LecturerDashboardPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoading: true,
+            isLoggedIn: false
+        }
+
+        // Set page display mode when loading
+        this.loadingStyle = {display: "none"}
+        this.loadedStyle = {display: ""}
+
+        // Get current router path
+    }
+    
+    componentDidMount() {
+        // Perform session check
+        AuthService.isLoggedIn()
+            .then(res => {
+                if (res.response && (res.response.status === 403))
+                    this.setState({
+                        isLoading: false,
+                        isLoggedIn: false
+                    });
+                else
+                    this.setState({
+                        isLoading: false,
+                        isLoggedIn: true
+                    })
+            });
+    }
+
+    render() {
+        if (!this.state.isLoggedIn && !this.state.isLoading) return <Redirect to="/"/>
+        const { path } = this.props.match;
+        return (
+            <MainWrapper>
+                <Preloader/>
+                <div style={this.state.isLoading ? this.loadingStyle : this.loadedStyle}>
+                    <Navbar />
+                    <Sidebar />
+                    <Switch>
+                        {/* Dashboard */}
+                        <Route exact path={path}><Dashboard/></Route>
+
+                        {/* Courses */}
+                        <Route exact path={`${path}/courses`}><Courses/></Route>
+                        {/* <Route exact path={`${path}/courses/:groupId`}><CourseList/></Route> */}
+                        <Route exact path={`${path}/courses/:semesterId/:classCode/:courseCode`}><Course/></Route>
+
+                        {/* Classes */}
+                        {/* <Route exact path={`${path}/classes`}><MajorList/></Route> */}
+                        {/* <Route exact path={`${path}/classes/:majorId`}><SemesterList/></Route> */}
+                        {/* <Route exact path={`${path}/classes/:majorId/:semesterId`}><ClassList/></Route> */}
+                        {/* <Route exact path={`${path}/classes/:majorId/:semesterId/:classId`}><ClassList/></Route> */}
+                        {/* <Route exact path={`${path}/classes/:majorId/:semesterId/:classId/:courseId`}><Class/></Route> */}
+                    </Switch>
+                    <PageWrapper><Footer/></PageWrapper>
+                </div>
+            </MainWrapper>
+        );
+    }
+}
+ 
+export default withRouter(LecturerDashboardPage);
