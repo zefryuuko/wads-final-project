0f9534f5fdcb43dc48946735a7b5a85e2abfec6e
Zef <me@zef.sh>
2020-05-31 18:37:31 +0700

    Clear account on remote when logging out

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
+import AuthService from '../services/AuthService';
+
 class Logout extends React.Component {  
+    UNSAFE_componentWillMount() {
+        const sessionId = localStorage.getItem('sessionId');
+        const universalId = localStorage.getItem('universalId');
+    
+        if (sessionId && universalId) {
+            AuthService.logout();
+        }
+    }
+
     render() {
         return (
             <script>{localStorage.clear()} {window.location.href = "/"}</script>
