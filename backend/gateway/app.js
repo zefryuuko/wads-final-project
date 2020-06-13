ed57357932ef2bd2986fd686c3f86cb50987d525
David Amadeo <ddsabiro@gmail.com>
2020-06-13 23:36:19 +0700

    CORS allow auth

--- backend/gateway/app.js
+++ backend/gateway/app.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
