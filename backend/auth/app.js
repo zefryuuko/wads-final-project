57cc6cf0b32e14dd95ab04e533a609bd3ffe321e
David Amadeo <ddsabiro@gmail.com>
2020-06-13 23:20:32 +0700

    Allow CORS

--- backend/auth/app.js
+++ backend/auth/app.js
@@ -7,6 +7,15 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Allow CORS
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 // Import Routes
 app.use('/account', require('./routes/account.route'));
 app.use('/session', require('./routes/session.route'));
